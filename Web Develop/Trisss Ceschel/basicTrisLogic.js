3fd2ad11be866926ee3bd03bc449cc03ce02d9cd
ceskelito <ceschelr@gmail.com>
2024-10-22 23:22:09 +0200

    Web Develop/Trisss

--- Web Develop/Trisss Ceschel/basicTrisLogic.js
+++ Web Develop/Trisss Ceschel/basicTrisLogic.js
@@ -1,15 +1,18 @@
+/*Function write/clear Grid*/
 function previewCell(cell)
 {
     if(cell.classList.contains("filled"))
         return;
-    let symbol =  document.getElementById("player").innerHTML;
-    let html = '<div class="symbol-' + symbol + '"  style = "opacity: 30%">'
-    cell.innerHTML = html;
+
+    let symbol =  document.getElementById("actualPlayer").innerHTML;
+    let img = '<img class="symbol" src="symbols/' + symbol + '.svg" alt="' + symbol + '" style="opacity: 30%">';
+    cell.innerHTML = img;
 }
 
 function clearCell(cell){
     if(cell.classList.contains("filled"))
         return;
+
     cell.innerHTML = "";
 }
 
@@ -18,22 +21,31 @@ function fillCell(cell){
     if(cell.classList.contains("filled"))
         return;
 
-    let symbol =  document.getElementById("player").innerHTML;
-    let html = '<div class="symbol-' + symbol + '">'
+    let symbol =  document.getElementById("actualPlayer").innerHTML;
+    let img = '<img class="symbol" src="symbols/' + symbol + '.svg" alt="' + symbol + '">';
+    cell.innerHTML = img;
 
-    cell.innerHTML = html;
     cell.classList.add("filled");
     cell.classList.add(symbol);
 
-
     if(checkVictory(cell) == true){
+        setTimeout(function(){
         window.alert("ha vinto il giocatore " + symbol);
         clearGrid();
+        }, 200)
         return;
     }
 
-    document.getElementById("player").innerHTML = (symbol == "x") ? "o" : "x";
+    document.getElementById("actualPlayer").innerHTML = (symbol == "cross") ? "circle" : "cross";
 }
+
+function clearGrid(){
+    removeElementsByClassName("symbol");
+    removeClassesByClassName("cell", ...["filled", "cross", "circle"]);
+}
+/*end of write/clear grid*/
+
+/*html interaction ByClassName*/
 function removeElementsByClassName(className){
     let elements = document.getElementsByClassName(className);
 
@@ -42,34 +54,30 @@ function removeElementsByClassName(className){
     }
 }
 
-function removeClassByClassName(className, classToRemove){
+function removeClassesByClassName(className, ...classesToRemove){
+
     let elements = document.getElementsByClassName(className);
 
     for(let element in elements){
-        elements[element].classList.remove("filled");
-        elements[element].classList.remove("x");
-        elements[element].classList.remove("o");
+        for(let classToRemove in classesToRemove){
+            elements[element].classList.remove(classesToRemove[classToRemove]);
+        }
     }
+   
 }
+/*end of html interaction ByClassName*/
 
-function clearGrid(){
-    removeElementsByClassName("symbol-x");
-    removeElementsByClassName("symbol-o");
-    removeClassByClassName("cell", "filled");
-}
 function checkVictory(cell){
 
     let id = cell.id[2] + cell.id[3];
     let tag = (a, b) => document.getElementById("c_" + a + b).classList[document.getElementById("c_" + a + b).classList.length-1];
-
-    console.log(tag(0,0));
 
     if((tag(id[0], 0) == tag(id[0], 1)) && (tag(id[0], 1) == tag(id[0], 2)) && (tag(id[0], 0) != ""))
         return true;
     if((tag(0, id[1]) == tag(1, id[1])) && (tag(1, id[1]) == tag(2, id[1])) && (tag(0, id[1]) != ""))
         return true;
 
-    if((tag(1, 1) == "x") || (tag(1, 1) == "o")){
+    if((tag(1, 1) == "cross") || (tag(1, 1) == "circle")){
         if((tag(0, 0) == tag(1, 1)) && (tag(1, 1) == tag(2, 2)))
             return true;
         if((tag(0, 2) == tag(1, 1)) && (tag(1, 1) == tag(2, 0)))
@@ -77,5 +85,3 @@ function checkVictory(cell){
     }
     return false;
 }
-
-
